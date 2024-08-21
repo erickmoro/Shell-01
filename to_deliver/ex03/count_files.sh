055ff143b9718d5e4766bfeb8182168188fc7411
So_Mais_Um <58540982+erickmoro@users.noreply.github.com>
2024-08-21 06:54:34 -0100

    Update count_files.sh

--- to_deliver/ex03/count_files.sh
+++ to_deliver/ex03/count_files.sh
@@ -1,3 +1,5 @@
 obijetivo Escreva uma linha de comando que mostre o número de arquivos regulares e de
 pastas dentro da pasta atual e todas as suas subpastas, incluindo o "."da pasta
 inicial.
+
+find . -type f | wc -l && find . -type d | wc -l
