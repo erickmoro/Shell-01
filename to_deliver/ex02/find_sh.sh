18e202144559f160224b4f9d4534b033c64d53c0
So_Mais_Um <58540982+erickmoro@users.noreply.github.com>
2024-08-21 06:43:01 -0100

    Create find_sh.sh

---
+++ to_deliver/ex02/find_sh.sh
@@ -0,0 +1,3 @@
+obijetivo • Escreva uma linha de comando que procure na pasta atual e em todas as subpastas
+todos os arquivos cujos nomes terminam com ".sh"(sem as aspas) e que só exiba os
+seus nomes, sem o .sh.
